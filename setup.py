dea2cdea28bfbc74b78256ee6dbb45a5b5e4a840
Ubuntu <ubuntu@ip-172-31-39-0.ec2.internal>
2024-08-08 10:13:38 +0000

    ingestion+processing+finetune workflow

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "doc-rag"
+AUTHOR_USER_NAME = "MutugiD"
+SRC_REPO = "dogRag"
+AUTHOR_EMAIL = "dennismutugi@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A python package for undertaking a document RAG",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # Corrected line
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
